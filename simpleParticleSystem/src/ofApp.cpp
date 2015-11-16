45dc848ddf4ce01c3ae3db1921e214dddeb9b16f
orekamenpe <orekamenpe@gmail.com>
2015-11-16 12:38:12 +0100

    draw a mouse following ball and its shadow

--- simpleParticleSystem/src/ofApp.cpp
+++ simpleParticleSystem/src/ofApp.cpp
@@ -2,17 +2,46 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+    // Smooth edges
+    ofEnableSmoothing();
+    
+    // Fixed framerate
+    ofSetFrameRate(30);
+    
+    // Initial x & y position of the ball
+    xPos = ofGetWindowWidth() * 0.5f;
+    yPos = ofGetWindowHeight() * 0.5f;
+    
+    oldXPos = xPos;
+    oldYPos = yPos;
 
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    
+    oldXPos = xPos;
+    oldYPos = yPos;
+    
+    // calculate the x and y distance
+    xPos += (mouseX - xPos) * 0.1f;
+    yPos += (mouseY - yPos) * 0.1f;
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    ofBackgroundGradient(ofColor::gray, ofColor(30,10,30), OF_GRADIENT_CIRCULAR);
+    
+    // Draw Shadow
+    ofSetColor(100,100,70);
+    ofFill();
+    ofDrawCircle(oldXPos,oldYPos,29);
+    
+    // Draw Circle
+    ofSetColor(200,200,124);
+    ofFill();
+    ofDrawCircle(xPos,yPos,30);
 }
 
 //--------------------------------------------------------------
@@ -37,6 +66,8 @@ void ofApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
+    xPos = ofRandom(ofGetWindowWidth());
+    yPos = ofRandom(ofGetWindowHeight());
 
 }
 
