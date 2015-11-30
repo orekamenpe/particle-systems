4607f6634aece99212c03250fbb524b57dba3c3f
orekamenpe <orekamenpe@gmail.com>
2015-12-01 00:36:19 +0100

    add new methods for the emitters

--- basicParticleSystem/src/FireEffect.cpp
+++ basicParticleSystem/src/FireEffect.cpp
@@ -10,6 +10,17 @@
 #include "ParticleUpdaters.h"
 #include "ParticleGenerators.h"
 
+namespace FireEffectConsts
+{
+    const ofVec3f kStartPosition = ofVec3f(300.0f, 400.0f, 0.0f);
+    const ofVec3f kYellowColor = ofVec3f{ 255, 255, 210 };
+    const ofVec3f kRedColor = ofVec3f{ 255, 10, 10};
+    
+    const ofVec3f kBrightYellowColor = ofVec3f{ 255, 255, 230 };
+}
+
+using namespace FireEffectConsts;
+
 bool FireEffect::initialize(size_t numParticles, ofImage glowImage, ofImage flame1Image, ofImage flame2Image, ofImage flame3Image, ofImage emberImage)
 {
     //
@@ -26,42 +37,30 @@ bool FireEffect::initialize(size_t numParticles, ofImage glowImage, ofImage flam
     auto particleEmitter = std::make_shared<ParticleEmitter>();
     {
         particleEmitter->_emitRate = 15;
-        
-        // position:
-        auto posGenerator = std::make_shared<generators::BoxPosGen>();
-        posGenerator->_pos = ofVec3f(300.0f, 400.0f, 0.0f);
-        particleEmitter->addGenerator(posGenerator);
+
+        fillEmitterPosition(*particleEmitter, kStartPosition);
         
         // color:
-        auto colGenerator = std::make_shared<generators::BasicColorGen>();
-        colGenerator->_minStartCol = ofVec3f{ 255, 255, 210 };
-        colGenerator->_maxStartCol = ofVec3f{ 255, 255, 210};
-        colGenerator->_minEndCol = ofVec3f{ 255, 10, 10};
-        colGenerator->_maxEndCol = ofVec3f{ 255, 10, 10};
-        particleEmitter->addGenerator(colGenerator);
-        
+        fillEmitterColor(*particleEmitter, kYellowColor, kYellowColor, kRedColor, kRedColor);
+
         // textures
-        auto imgGenerator = std::make_shared<generators::BasicImageGen>();
-        imgGenerator->_image = glowImage;
-        imgGenerator->_startSize = ofVec2f(glowImage.getWidth()*2.5f, glowImage.getHeight()*2.5f);
-        imgGenerator->_endSize = ofVec2f(glowImage.getWidth() * 1.0f, glowImage.getHeight() * 1.0f);
-        imgGenerator->_startOpacity = 15;
-        imgGenerator->_endOpacity = 5;
-        particleEmitter->addGenerator(imgGenerator);
+        ofVec2f startSize = ofVec2f(glowImage.getWidth()*2.5f, glowImage.getHeight()*2.5f);
+        ofVec2f endSize = ofVec2f(glowImage.getWidth(), glowImage.getHeight());
+        float startOpacity = 15;
+        float endOpacity = 5;
+        fillEmitterTextures(*particleEmitter, glowImage, startSize, endSize, startOpacity, endOpacity);
         
         // velocity
-        auto velGenerator = std::make_shared<generators::BasicVelGen>();
-        velGenerator->_minStartVel = ofVec3f{ 1.25f, 0.0f, 0.0f};
-        velGenerator->_maxStartVel = velGenerator->_minStartVel;
-        velGenerator->_direction = -90.0f;
-        //velGenerator->_directionDev = 20.0f;
-        particleEmitter->addGenerator(velGenerator);
+        ofVec3f minStartVel = ofVec3f{ 1.25f, 0.0f, 0.0f};
+        ofVec3f maxStartVel = minStartVel;
+        float direction = -90.f;
+        float directionDev = 0;
+        fillEmitterVelocity(*particleEmitter, minStartVel, maxStartVel,  direction, directionDev);
         
         // time
-        auto timeGenerator = std::make_shared<generators::BasicTimeGen>();
-        timeGenerator->_minTime = 80;
-        timeGenerator->_maxTime = 80;
-        particleEmitter->addGenerator(timeGenerator);
+        float minTime = 80;
+        float maxTime = 80;
+        fillEmitterTime(*particleEmitter, minTime, maxTime);
     }
     
     auto glowEmitter = std::make_shared<ParticleEmitter>();
@@ -69,81 +68,61 @@ bool FireEffect::initialize(size_t numParticles, ofImage glowImage, ofImage flam
         glowEmitter->_emitRate = 15;
         
         // position:
-        auto posGenerator = std::make_shared<generators::BoxPosGen>();
-        posGenerator->_pos = ofVec3f(300.0f, 450.0f, 0.0f);
-        glowEmitter->addGenerator(posGenerator);
+        ofVec3f startPosition = kStartPosition;
+        startPosition.y += 50;
+        fillEmitterPosition(*glowEmitter, startPosition);
         
         // color:
-        auto colGenerator = std::make_shared<generators::BasicColorGen>();
-        colGenerator->_minStartCol = ofVec3f{ 255, 255, 230 };
-        colGenerator->_maxStartCol = ofVec3f{ 255, 255, 230};
-        colGenerator->_minEndCol = ofVec3f{ 255, 255, 210};
-        colGenerator->_maxEndCol = ofVec3f{ 255, 255, 210};
-        glowEmitter->addGenerator(colGenerator);
+        fillEmitterColor(*glowEmitter, kBrightYellowColor, kBrightYellowColor, kYellowColor, kYellowColor);
         
         // textures
-        auto imgGenerator = std::make_shared<generators::BasicImageGen>();
-        imgGenerator->_image = glowImage;
-        imgGenerator->_startSize = ofVec2f(glowImage.getWidth()*1.8f, glowImage.getHeight()*1.0f);
-        imgGenerator->_endSize = ofVec2f(glowImage.getWidth() * 0.1f, glowImage.getHeight() * 0.3f);
-        imgGenerator->_startOpacity = 200;
-        imgGenerator->_endOpacity = 5;
-        glowEmitter->addGenerator(imgGenerator);
+        ofVec2f startSize = ofVec2f(glowImage.getWidth()*1.9f, glowImage.getHeight()*1.0f);
+        ofVec2f endSize = ofVec2f(glowImage.getWidth()*0.1f, glowImage.getHeight()*0.3f);
+        float startOpacity = 200;
+        float endOpacity = 5;
+        fillEmitterTextures(*glowEmitter, glowImage, startSize, endSize, startOpacity, endOpacity);
         
         // velocity
-        auto velGenerator = std::make_shared<generators::BasicVelGen>();
-        velGenerator->_minStartVel = ofVec3f{ 1.25f, 0.0f, 0.0f};
-        velGenerator->_maxStartVel = velGenerator->_minStartVel;
-        velGenerator->_direction = -90.0f;
-        //velGenerator->_directionDev = 20.0f;
-        glowEmitter->addGenerator(velGenerator);
+        ofVec3f minStartVel = ofVec3f{ 0.25f, 0.0f, 0.0f};
+        ofVec3f maxStartVel = minStartVel;
+        float direction = -90.f;
+        float directionDev = 0;
+        fillEmitterVelocity(*glowEmitter, minStartVel, maxStartVel,  direction, directionDev);
         
         // time
-        auto timeGenerator = std::make_shared<generators::BasicTimeGen>();
-        timeGenerator->_minTime = 70;
-        timeGenerator->_maxTime = 70;
-        glowEmitter->addGenerator(timeGenerator);
+        float minTime = 30;
+        float maxTime = 30;
+        fillEmitterTime(*glowEmitter, minTime, maxTime);
     }
-    
+
     auto flame1ParticleEmitter = std::make_shared<ParticleEmitter>();
     {
         flame1ParticleEmitter->_emitRate = 3;
         
         // position:
-        auto posGenerator = std::make_shared<generators::BoxPosGen>();
-        posGenerator->_pos = ofVec3f(300.0f, 400.0f, 0.0f);
-        flame1ParticleEmitter->addGenerator(posGenerator);
+        fillEmitterPosition(*flame1ParticleEmitter, kStartPosition);
         
         // color:
-        auto colGenerator = std::make_shared<generators::BasicColorGen>();
-        colGenerator->_minStartCol = ofVec3f{ 255, 255, 210 };
-        colGenerator->_maxStartCol = ofVec3f{ 255, 255, 210};
-        colGenerator->_minEndCol = ofVec3f{ 255, 10, 10};
-        colGenerator->_maxEndCol = ofVec3f{ 255, 10, 10};
-        flame1ParticleEmitter->addGenerator(colGenerator);
+        fillEmitterColor(*flame1ParticleEmitter, kYellowColor, kYellowColor, kRedColor, kRedColor);
         
         // textures
-        auto imgGenerator = std::make_shared<generators::BasicImageGen>();
-        imgGenerator->_image = flame1Image;
-        imgGenerator->_startSize = ofVec2f(flame1Image.getWidth() * 0.5f, flame1Image.getHeight() * 0.5f);
-        imgGenerator->_endSize = ofVec2f(flame1Image.getWidth() * 0.15f, flame1Image.getHeight() * 0.15f);
-        imgGenerator->_startOpacity = 200;
-        imgGenerator->_endOpacity = 0;
-        flame1ParticleEmitter->addGenerator(imgGenerator);
+        ofVec2f startSize = ofVec2f(flame1Image.getWidth()*0.5f, flame1Image.getHeight()*0.5f);
+        ofVec2f endSize = ofVec2f(flame1Image.getWidth()*0.15f, flame1Image.getHeight()*0.15f);
+        float startOpacity = 200;
+        float endOpacity = 0;
+        fillEmitterTextures(*flame1ParticleEmitter, flame1Image, startSize, endSize, startOpacity, endOpacity);
         
         // velocity
-        auto velGenerator = std::make_shared<generators::BasicVelGen>();
-        velGenerator->_minStartVel = ofVec3f{ 2.5f, 0.0f, 0.0f};
-        velGenerator->_maxStartVel = velGenerator->_minStartVel;
-        velGenerator->_direction = -90.0f;
-        velGenerator->_directionDev = 5.0f;
-        flame1ParticleEmitter->addGenerator(velGenerator);
+        ofVec3f minStartVel = ofVec3f{ 2.5f, 0.0f, 0.0f};
+        ofVec3f maxStartVel = minStartVel;
+        float direction = -90.f;
+        float directionDev = 5;
+        fillEmitterVelocity(*flame1ParticleEmitter, minStartVel, maxStartVel,  direction, directionDev);
         
         // time
-        auto timeGenerator = std::make_shared<generators::BasicTimeGen>();
-        timeGenerator->_minTime = 30;
-        timeGenerator->_maxTime = 30;
-        flame1ParticleEmitter->addGenerator(timeGenerator);
+        float minTime = 30;
+        float maxTime = 30;
+        fillEmitterTime(*flame1ParticleEmitter, minTime, maxTime);
     }
     
     auto flame2ParticleEmitter = std::make_shared<ParticleEmitter>();
@@ -151,40 +130,29 @@ bool FireEffect::initialize(size_t numParticles, ofImage glowImage, ofImage flam
         flame2ParticleEmitter->_emitRate = 4;
         
         // position:
-        auto posGenerator = std::make_shared<generators::BoxPosGen>();
-        posGenerator->_pos = ofVec3f(300.0f, 400.0f, 0.0f);
-        flame2ParticleEmitter->addGenerator(posGenerator);
+        fillEmitterPosition(*flame2ParticleEmitter, kStartPosition);
         
         // color:
-        auto colGenerator = std::make_shared<generators::BasicColorGen>();
-        colGenerator->_minStartCol = ofVec3f{ 255, 255, 210 };
-        colGenerator->_maxStartCol = ofVec3f{ 255, 255, 210};
-        colGenerator->_minEndCol = ofVec3f{ 255, 10, 10};
-        colGenerator->_maxEndCol = ofVec3f{ 255, 10, 10};;
-        flame2ParticleEmitter->addGenerator(colGenerator);
+        fillEmitterColor(*flame2ParticleEmitter, kYellowColor, kYellowColor, kRedColor, kRedColor);
         
         // textures
-        auto imgGenerator = std::make_shared<generators::BasicImageGen>();
-        imgGenerator->_image = flame2Image;
-        imgGenerator->_startSize = ofVec2f(flame2Image.getWidth() * 0.5f, flame2Image.getHeight() * 0.5f);
-        imgGenerator->_endSize = ofVec2f(flame2Image.getWidth() * 0.15f, flame2Image.getHeight() * 0.15f);
-        imgGenerator->_startOpacity = 200;
-        imgGenerator->_endOpacity = 0;
-        flame2ParticleEmitter->addGenerator(imgGenerator);
+        ofVec2f startSize = ofVec2f(flame2Image.getWidth()*0.5f, flame2Image.getHeight()*0.5f);
+        ofVec2f endSize = ofVec2f(flame2Image.getWidth()*0.15f, flame2Image.getHeight()*0.15f);
+        float startOpacity = 200;
+        float endOpacity = 0;
+        fillEmitterTextures(*flame2ParticleEmitter, flame2Image, startSize, endSize, startOpacity, endOpacity);
         
         // velocity
-        auto velGenerator = std::make_shared<generators::BasicVelGen>();
-        velGenerator->_minStartVel = ofVec3f{ 2.5f, 0.0f, 0.0f};
-        velGenerator->_maxStartVel = velGenerator->_minStartVel;
-        velGenerator->_direction = -90.0f;
-        velGenerator->_directionDev = 5.0f;
-        flame2ParticleEmitter->addGenerator(velGenerator);
+        ofVec3f minStartVel = ofVec3f{ 2.5f, 0.0f, 0.0f};
+        ofVec3f maxStartVel = minStartVel;
+        float direction = -90.f;
+        float directionDev = 5;
+        fillEmitterVelocity(*flame2ParticleEmitter, minStartVel, maxStartVel,  direction, directionDev);
         
         // time
-        auto timeGenerator = std::make_shared<generators::BasicTimeGen>();
-        timeGenerator->_minTime = 30;
-        timeGenerator->_maxTime = 30;
-        flame2ParticleEmitter->addGenerator(timeGenerator);
+        float minTime = 30;
+        float maxTime = 30;
+        fillEmitterTime(*flame2ParticleEmitter, minTime, maxTime);
     }
     
     auto flame3ParticleEmitter = std::make_shared<ParticleEmitter>();
@@ -192,42 +160,31 @@ bool FireEffect::initialize(size_t numParticles, ofImage glowImage, ofImage flam
         flame3ParticleEmitter->_emitRate = 5;
         
         // position:
-        auto posGenerator = std::make_shared<generators::BoxPosGen>();
-        posGenerator->_pos = ofVec3f(300.0f, 400.0f, 0.0f);
-        flame3ParticleEmitter->addGenerator(posGenerator);
+        fillEmitterPosition(*flame3ParticleEmitter, kStartPosition);
         
         // color:
-        auto colGenerator = std::make_shared<generators::BasicColorGen>();
-        colGenerator->_minStartCol = ofVec3f{ 255, 255, 210 };
-        colGenerator->_maxStartCol = ofVec3f{ 255, 255, 210};
-        colGenerator->_minEndCol = ofVec3f{ 255, 10, 10};
-        colGenerator->_maxEndCol = ofVec3f{ 255, 10, 10};;
-        flame3ParticleEmitter->addGenerator(colGenerator);
+        fillEmitterColor(*flame3ParticleEmitter, kYellowColor, kYellowColor, kRedColor, kRedColor);
         
         // textures
-        auto imgGenerator = std::make_shared<generators::BasicImageGen>();
-        imgGenerator->_image = flame3Image;
-        imgGenerator->_startSize = ofVec2f(flame3Image.getWidth() * 0.5f, flame3Image.getHeight() * 0.5f);
-        imgGenerator->_endSize = ofVec2f(flame3Image.getWidth() * 0.15f, flame3Image.getHeight() * 0.15f);
-        imgGenerator->_startOpacity = 200;
-        imgGenerator->_endOpacity = 0;
-        flame3ParticleEmitter->addGenerator(imgGenerator);
+        ofVec2f startSize = ofVec2f(flame3Image.getWidth()*0.5f, flame3Image.getHeight()*0.5f);
+        ofVec2f endSize = ofVec2f(flame3Image.getWidth()*0.15f, flame3Image.getHeight()*0.15f);
+        float startOpacity = 200;
+        float endOpacity = 0;
+        fillEmitterTextures(*flame3ParticleEmitter, flame3Image, startSize, endSize, startOpacity, endOpacity);
         
         // velocity
-        auto velGenerator = std::make_shared<generators::BasicVelGen>();
-        velGenerator->_minStartVel = ofVec3f{ 2.5f, 0.0f, 0.0f};
-        velGenerator->_maxStartVel = velGenerator->_minStartVel;
-        velGenerator->_direction = -90.0f;
-        velGenerator->_directionDev = 5.0f;
-        flame3ParticleEmitter->addGenerator(velGenerator);
+        ofVec3f minStartVel = ofVec3f{ 2.5f, 0.0f, 0.0f};
+        ofVec3f maxStartVel = minStartVel;
+        float direction = -90.f;
+        float directionDev = 5;
+        fillEmitterVelocity(*flame3ParticleEmitter, minStartVel, maxStartVel,  direction, directionDev);
         
         // time
-        auto timeGenerator = std::make_shared<generators::BasicTimeGen>();
-        timeGenerator->_minTime = 30;
-        timeGenerator->_maxTime = 30;
-        flame3ParticleEmitter->addGenerator(timeGenerator);
+        float minTime = 30;
+        float maxTime = 30;
+        fillEmitterTime(*flame3ParticleEmitter, minTime, maxTime);
     }
-    
+
     auto emberParticleEmitter = std::make_shared<ParticleEmitter>();
     {
         emberParticleEmitter->_emitRate = 1;
@@ -303,6 +260,51 @@ bool FireEffect::initialize(size_t numParticles, ofImage glowImage, ofImage flam
     return true;
 }
 
+void FireEffect::fillEmitterPosition(ParticleEmitter& emitter, const ofVec3f& startPosition)
+{
+    auto posGenerator = std::make_shared<generators::BoxPosGen>();
+    posGenerator->_pos = startPosition;
+    emitter.addGenerator(posGenerator);
+}
+
+void FireEffect::fillEmitterColor(ParticleEmitter& emitter, const ofVec3f& minStartColor, const ofVec3f& maxStartColor, const ofVec3f& minEndColor, const ofVec3f& maxEndColor)
+{
+    auto colGenerator = std::make_shared<generators::BasicColorGen>();
+    colGenerator->_minStartCol = minStartColor;
+    colGenerator->_maxStartCol = maxStartColor;
+    colGenerator->_minEndCol = minEndColor;
+    colGenerator->_maxEndCol = maxEndColor;
+    emitter.addGenerator(colGenerator);
+}
+
+void FireEffect::fillEmitterTextures(ParticleEmitter& emitter, const ofImage& image, const ofVec2f& startSize, const ofVec2f& endSize, float startOpacity, float endOpacity)
+{
+    auto imgGenerator = std::make_shared<generators::BasicImageGen>();
+    imgGenerator->_image = image;
+    imgGenerator->_startSize = startSize;
+    imgGenerator->_endSize = endSize;
+    imgGenerator->_startOpacity = startOpacity;
+    imgGenerator->_endOpacity = endOpacity;
+    emitter.addGenerator(imgGenerator);
+}
+
+void FireEffect::fillEmitterVelocity(ParticleEmitter& emitter, const ofVec3f& minStartVel, const ofVec3f& maxStartVel, float direction, float directionDev)
+{
+    auto velGenerator = std::make_shared<generators::BasicVelGen>();
+    velGenerator->_minStartVel = minStartVel;
+    velGenerator->_maxStartVel = maxStartVel;
+    velGenerator->_direction = direction;
+    velGenerator->_directionDev = directionDev;
+    emitter.addGenerator(velGenerator);
+}
+
+void FireEffect::fillEmitterTime(ParticleEmitter& emitter, float minTime, float maxTime)
+{
+    auto timeGenerator = std::make_shared<generators::BasicTimeGen>();
+    timeGenerator->_minTime = minTime;
+    timeGenerator->_maxTime = maxTime;
+    emitter.addGenerator(timeGenerator);
+}
 
 void FireEffect::clean()
 {
--- basicParticleSystem/src/FireEffect.h
+++ basicParticleSystem/src/FireEffect.h
@@ -23,6 +23,13 @@ private:
     std::shared_ptr<ParticleSystem> _glowSystem;
     std::shared_ptr<ParticleSystem> _emberSystem;
     
+    void fillEmitterPosition(ParticleEmitter& emitter, const ofVec3f& startPosition);
+    void fillEmitterColor(ParticleEmitter& emitter, const ofVec3f& minStartColor, const ofVec3f& maxStartColor, const ofVec3f& minEndColor, const ofVec3f& maxEndColor);
+    void fillEmitterTextures(ParticleEmitter& emitter, const ofImage& image, const ofVec2f& startSize, const ofVec2f& endSize, float startOpacity, float endOpacity);
+    void fillEmitterVelocity(ParticleEmitter& emitter, const ofVec3f& minStartVel, const ofVec3f& maxStartVel, float direction, float directionDev);
+    void fillEmitterTime(ParticleEmitter& emitter, float minTime, float maxTime);
+
+    
 public:
     FireEffect() { }
     ~FireEffect() { }
